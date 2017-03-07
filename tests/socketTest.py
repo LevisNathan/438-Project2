edf8f47c179b8904211b16dadfdfaf143c5d6517
Nathan K Levis <levisnathan@gmail.com>
2017-03-07 03:41:40 +0000

    socket test disabled

--- tests/socketTest.py
+++ tests/socketTest.py
@@ -1,20 +1,20 @@
 import unittest
 import app
 
-class socket_tests(unittest.TestCase):
-    def test_hello(self):
-        client = app.socketio.test_client(app.app)
-        responce =client.get_received()
-        print responce
+# class socket_tests(unittest.TestCase):
+    # def test_hello(self):
+    #     client = app.socketio.test_client(app.app)
+    #     responce =client.get_received()
+    #     print responce
         
-    def test_emit(self):
-        client = app.socketio.test_client(app.app)
-        client.emit('new message', {
-            'message':'this is a client massage'
-        })
-        r = client.get_received()
-        for i in r:
-            print i
+    # def test_emit(self):
+    #     client = app.socketio.test_client(app.app)
+    #     client.emit('new message', {
+    #         'message':'this is a client massage'
+    #     })
+    #     r = client.get_received()
+    #     for i in r:
+    #         print i
 
 if __name__ == '__main__':
     unittest.main()
